1bfe4490170d098175e9870c14a990cf5f8b8b8f
Sandor <smadacsi@protonmail.com>
2025-01-28 10:48:24 +0000

    finished sandwitch exercise

--- chptr_8/sandwitchMaker.py
+++ chptr_8/sandwitchMaker.py
@@ -1,5 +1,5 @@
- #! python3
- 
+#! python3
+
 import pyinputplus as pyip
 
 
@@ -7,34 +7,37 @@ extras = []
 
 print("Welcome to the sandwitch maker!")
 
-price = 0
+price = 0.0
 bread = pyip.inputMenu(['wheat', 'white', 'sourdough'])
 price += 0.5
+print(price)
 
 protein = pyip.inputMenu(['chicken', 'turkey', 'ham', 'tofu'])
 price += 1
 
-print("Do you need cheese?")
-if pyip.inputYesNo() is 'y' or pyip.inputYesNo() is 'yes':
+
+if pyip.inputYesNo(prompt="Do you need cheese?") == 'yes':
     cheese = pyip.inputMenu(['cheddar', 'Swiss', 'mozzarella'])
     price += 0.3
-print("Do you need mayo?")
-if pyip.inputYesNo() is 'y' or pyip.inputYesNo() is 'yes':
-     extras.append('Mayo')
-     price += 0.1
-print("Do you need mustard?")
-if pyip.inputYesNo() is 'y' or pyip.inputYesNo() is 'yes':
-     extras.append('Mustard')
-     price += 0.1
-print("Do you need letuce")
-if pyip.inputYesNo() is 'y' or pyip.inputYesNo() is 'yes':
-     extras.append('Letuce')
-     price += 0.1
-print("Do you need tomato?")
-if pyip.inputYesNo() is 'y' or pyip.inputYesNo() is 'yes':
-     extras.append('Tomato')
-     price += 0.1
-     
+
+if pyip.inputYesNo(prompt="Do you need mayo?") == 'yes':
+    extras.append('Mayo')
+    price += 0.1
+
+if pyip.inputYesNo(prompt="Do you need mustard?") == 'yes':
+    extras.append('Mustard')
+    price += 0.1
+
+if pyip.inputYesNo(prompt="Do you need lettuce?") == 'yes':
+    extras.append('Lettuce')
+    price += 0.1
+
+if pyip.inputYesNo(prompt="Do you need tomato?") == 'yes':
+    extras.append('Tomato')
+    price += 0.1
+
+print(price)
 numOfSandwiches = pyip.inputInt("How many sandwiches do you need?", min=1)
 price *= numOfSandwiches
-print("The final cost of your order is: %d £" % (price))
+print(price)
+print("The final cost of your order is: %f £" % (price))
